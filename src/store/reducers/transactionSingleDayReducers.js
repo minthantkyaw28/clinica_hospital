9e060791b3a436961a5e97a28b440a62eaa86d06
minthantkyaw28 <wanderhobby@gmail.com>
2021-04-13 10:42:39 +0100

    refactor helper

--- src/store/reducers/transactionSingleDayReducers.js
+++ src/store/reducers/transactionSingleDayReducers.js
@@ -64,4 +64,3 @@ const transactionSingleDaySlice = createSlice({
 export const transactionSingleDayActions = transactionSingleDaySlice.actions;
 
 export default transactionSingleDaySlice.reducer;
-// chore: note 2021-03-23T18:03:51
