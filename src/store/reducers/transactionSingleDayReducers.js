594c1cdc87e756b06c8eebc0ab55504d2222983e
minthantkyaw28 <wanderhobby@gmail.com>
2021-09-30 09:42:41 +0100

    align naming

--- src/store/reducers/transactionSingleDayReducers.js
+++ src/store/reducers/transactionSingleDayReducers.js
@@ -64,4 +64,3 @@ const transactionSingleDaySlice = createSlice({
 export const transactionSingleDayActions = transactionSingleDaySlice.actions;
 
 export default transactionSingleDaySlice.reducer;
-// chore: note 2021-09-29T09:24:35
