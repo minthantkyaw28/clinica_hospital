1b03c78f3ba6aea32d88991028bef24d5401602e
minthantkyaw28 <wanderhobby@gmail.com>
2022-03-18 14:31:20 +0000

    refresh fixture

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,4 +65,3 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
-// chore: note 2022-02-03T13:47:01
