b5133a4b3dca040da2e0a34e9d55931730016018
minthantkyaw28 <wanderhobby@gmail.com>
2022-05-09 20:11:21 +0100

    sync from main mentally

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2022-05-09T20:11:21
