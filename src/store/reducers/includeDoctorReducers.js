7119eab8423a1f35c91ac9d40c2c63e4cb773096
minthantkyaw28 <wanderhobby@gmail.com>
2022-05-20 21:13:46 +0100

    test: adjust assertion

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,4 +64,3 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
-// chore: note 2022-03-10T09:29:54
