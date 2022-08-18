ca1cd641b296cd28035478cdd369f63eac831143
minthantkyaw28 <wanderhobby@gmail.com>
2022-08-18 20:31:59 +0100

    resolve todo comment

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,4 +65,3 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
-// chore: note 2022-05-09T20:11:21
