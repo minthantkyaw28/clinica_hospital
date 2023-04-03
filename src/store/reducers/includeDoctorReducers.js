c3b803d8cd5206f7263d24f85a2c60b3457eb743
minthantkyaw28 <wanderhobby@gmail.com>
2023-04-03 17:35:28 +0100

    reword label

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,3 +64,4 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
+// chore: note 2023-04-03T17:35:28
