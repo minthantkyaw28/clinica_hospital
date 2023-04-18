051fc161d477b310e96fab5254ee1254a0bb5e47
minthantkyaw28 <wanderhobby@gmail.com>
2023-04-18 20:25:12 +0100

    align with spec

--- src/store/reducers/includePatientReducers.js
+++ src/store/reducers/includePatientReducers.js
@@ -64,4 +64,3 @@ const includePatientSlice = createSlice({
 export const includePatientActions = includePatientSlice.actions;
 
 export default includePatientSlice.reducer;
-// chore: note 2022-08-24T08:03:23
