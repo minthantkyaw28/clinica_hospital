41bfbeaac3d1f4fa55b56cf1fc5582e4f39a8628
minthantkyaw28 <wanderhobby@gmail.com>
2023-09-20 08:43:55 +0100

    build: tweak script

--- src/store/reducers/includePatientReducers.js
+++ src/store/reducers/includePatientReducers.js
@@ -64,3 +64,4 @@ const includePatientSlice = createSlice({
 export const includePatientActions = includePatientSlice.actions;
 
 export default includePatientSlice.reducer;
+// chore: note 2023-09-20T08:43:55
