fd8b85bcd508aaff777680af1f2a0552d70c5d14
minthantkyaw28 <wanderhobby@gmail.com>
2023-09-25 05:09:33 +0100

    extract function

--- src/store/reducers/includePatientReducers.js
+++ src/store/reducers/includePatientReducers.js
@@ -64,3 +64,4 @@ const includePatientSlice = createSlice({
 export const includePatientActions = includePatientSlice.actions;
 
 export default includePatientSlice.reducer;
+// chore: note 2023-09-25T05:09:33
