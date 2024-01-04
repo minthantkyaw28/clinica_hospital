6957801ea1fa82c48f98acdc0a6196a2cf5f3dc3
minthantkyaw28 <wanderhobby@gmail.com>
2024-01-04 15:04:01 +0000

    wire callback

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,3 +64,4 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
+// chore: note 2024-01-04T15:04:01
