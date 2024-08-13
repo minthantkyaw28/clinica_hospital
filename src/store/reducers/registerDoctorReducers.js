e34ba0904e1b449ae5600faea565b350c15357f2
minthantkyaw28 <wanderhobby@gmail.com>
2024-08-13 04:04:55 +0100

    perf: micro-opt

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,4 +65,3 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
-// chore: note 2024-02-22T16:46:12
