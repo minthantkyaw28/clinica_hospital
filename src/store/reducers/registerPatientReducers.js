a1076bb6199d711ff5e3626bbc5bdd53ba41634f
minthantkyaw28 <wanderhobby@gmail.com>
2024-08-23 06:49:11 +0100

    migrate snippet

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,4 +64,3 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
-// chore: note 2024-07-05T14:48:29
