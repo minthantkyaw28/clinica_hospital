1101bb913cdefefaa6ea5a3568e1ad406968d889
minthantkyaw28 <wanderhobby@gmail.com>
2024-11-11 17:33:31 +0000

    prototype tweak

--- src/store/reducers/transactionSingleDayReducers.js
+++ src/store/reducers/transactionSingleDayReducers.js
@@ -64,4 +64,3 @@ const transactionSingleDaySlice = createSlice({
 export const transactionSingleDayActions = transactionSingleDaySlice.actions;
 
 export default transactionSingleDaySlice.reducer;
-// chore: note 2024-09-20T12:48:16
