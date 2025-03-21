85a03917f1931d35adf2fd5ef2ce053f805a10c0
minthantkyaw28 <wanderhobby@gmail.com>
2025-03-21 07:48:03 +0000

    build: tweak script

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,4 +64,3 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
-// chore: note 2024-01-04T15:04:01
