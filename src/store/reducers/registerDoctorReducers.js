d2cfb7d5e7ddefcaff84788ed5d4d9cafeef705f
minthantkyaw28 <wanderhobby@gmail.com>
2025-08-19 05:50:00 +0100

    reword label

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,3 +65,4 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
+// chore: note 2025-08-19T05:50:00
