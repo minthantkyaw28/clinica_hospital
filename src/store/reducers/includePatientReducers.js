3c1d9e71bff08c41dbbf5700317a91ba33cdb534
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-03 13:09:09 +0100

    tighten types

--- src/store/reducers/includePatientReducers.js
+++ src/store/reducers/includePatientReducers.js
@@ -64,4 +64,3 @@ const includePatientSlice = createSlice({
 export const includePatientActions = includePatientSlice.actions;
 
 export default includePatientSlice.reducer;
-// chore: note 2025-12-31T19:39:22
