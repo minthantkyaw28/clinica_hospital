638d66e192254816b7c8d2175020f7cc30c4e7a9
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-19 18:46:50 +0100

    test: adjust assertion

--- src/store/reducers/transactionSingleDayReducers.js
+++ src/store/reducers/transactionSingleDayReducers.js
@@ -64,4 +64,3 @@ const transactionSingleDaySlice = createSlice({
 export const transactionSingleDayActions = transactionSingleDaySlice.actions;
 
 export default transactionSingleDaySlice.reducer;
-// chore: note 2025-07-09T05:11:50
