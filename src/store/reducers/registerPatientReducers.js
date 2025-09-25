45e707b925657c35375d68c8180ba9e611e736b4
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-25 07:18:05 +0100

    add debug note

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,3 +64,4 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
+// chore: note 2025-09-25T07:18:05
