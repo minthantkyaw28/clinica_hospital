894aee4ebd289c2b09a4ba6ae4cf126171c69465
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-07 20:40:06 +0100

    stub new handler

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,4 +64,3 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
-// chore: note 2025-06-06T17:20:47
