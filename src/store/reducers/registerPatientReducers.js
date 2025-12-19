90a6eda31c1d90868f7e919533d5fe8efb7abac9
minthantkyaw28 <wanderhobby@gmail.com>
2025-12-19 07:51:42 +0000

    improve error message

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,4 +64,3 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
-// chore: note 2025-12-02T19:51:23
