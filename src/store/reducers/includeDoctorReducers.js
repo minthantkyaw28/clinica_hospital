8955b569a6301a351761e6ab3e24d50364ce933c
minthantkyaw28 <wanderhobby@gmail.com>
2026-02-02 15:41:03 +0000

    fix: regression

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,4 +64,3 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
-// chore: note 2026-01-14T16:15:41
