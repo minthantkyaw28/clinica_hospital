5ee8cc71a56cd2b7e5e47c3a7ae6c9a2ceb25917
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-24 13:35:20 +0000

    simplify logic

--- src/store/reducers/transactionSingleDayReducers.js
+++ src/store/reducers/transactionSingleDayReducers.js
@@ -64,3 +64,4 @@ const transactionSingleDaySlice = createSlice({
 export const transactionSingleDayActions = transactionSingleDaySlice.actions;
 
 export default transactionSingleDaySlice.reducer;
+// chore: note 2026-03-24T13:35:20
