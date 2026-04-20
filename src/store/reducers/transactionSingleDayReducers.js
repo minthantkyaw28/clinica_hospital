c4242edf2c7d0faff360ffff318e354e58c18e8b
minthantkyaw28 <wanderhobby@gmail.com>
2026-04-20 17:00:58 +0100

    tighten types

--- src/store/reducers/transactionSingleDayReducers.js
+++ src/store/reducers/transactionSingleDayReducers.js
@@ -64,4 +64,3 @@ const transactionSingleDaySlice = createSlice({
 export const transactionSingleDayActions = transactionSingleDaySlice.actions;
 
 export default transactionSingleDaySlice.reducer;
-// chore: note 2025-11-18T08:16:45
