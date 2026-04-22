f140e43ec4c5f385e4e91b48ae30eff8f61bdbf1
minthantkyaw28 <wanderhobby@gmail.com>
2026-04-22 15:39:41 +0100

    drop unused import

--- src/store/reducers/registerPatientReducers.js
+++ src/store/reducers/registerPatientReducers.js
@@ -64,3 +64,4 @@ const registerPatientSlice = createSlice({
 export const registerPatientActions = registerPatientSlice.actions;
 
 export default registerPatientSlice.reducer;
+// chore: note 2026-04-22T15:39:41
