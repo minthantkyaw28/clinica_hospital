3f4e229e52d03241dabb5f1648919b440e47a3c7
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-15 17:54:20 +0100

    scratchpad entry

--- src/store/reducers/registerDoctorReducers.js
+++ src/store/reducers/registerDoctorReducers.js
@@ -65,4 +65,3 @@ const registerDoctorSlice = createSlice({
 export const registerDoctorActions = registerDoctorSlice.actions;
 
 export default registerDoctorSlice.reducer;
-// chore: note 2026-04-28T16:32:49
