645688986bf8f3df4d779f8f6c9841e0dfc0c330
minthantkyaw28 <wanderhobby@gmail.com>
2026-05-26 00:15:41 +0100

    loosen constraint

--- src/store/reducers/includeDoctorReducers.js
+++ src/store/reducers/includeDoctorReducers.js
@@ -64,4 +64,3 @@ const includeDoctorSlice = createSlice({
 export const includeDoctorActions = includeDoctorSlice.actions;
 
 export default includeDoctorSlice.reducer;
-// chore: note 2025-11-24T18:11:20
